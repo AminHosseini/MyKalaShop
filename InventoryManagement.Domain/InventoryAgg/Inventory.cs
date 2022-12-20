e93d46f626f05f8c40b094bf5782ab393442ec7d
AminHosseini <aminhosseini741@gmail.com>
2022-12-20 17:38:14 +0330

    Inventory Model Implemented

---
+++ InventoryManagement.Domain/InventoryAgg/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using _0_Framework.Domain;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public class Inventory : EntityBase
+    {
+        public long ProductId { get; private set; }
+        public int UnitPrice { get; private set; }
+        public string Color { get; private set; }
+        public bool IsAvailable { get; private set; }
+        public List<InventoryOperation> Operations { get; private set; }
+
+        public Inventory(long productId, int unitPrice, string color)
+        {
+            ProductId = productId;
+            UnitPrice = unitPrice;
+            Color = color;
+            IsAvailable = false;
+            Operations = new List<InventoryOperation>();
+        }
+
+        public void Edit(long productId, int unitPrice, string color)
+        {
+            ProductId = productId;
+            UnitPrice = unitPrice;
+            Color = color;
+        }
+
+        public int CalculateCurrentCount()
+        {
+            var added = Operations.Where(x => x.Added).Sum(x => x.Count);
+            var removed = Operations.Where(x => !x.Added).Sum(x => x.Count);
+            return added - removed;
+        }
+
+        public void Increase(int count, long operatorId, string description)
+        {
+            var currentCount = CalculateCurrentCount() + count;
+            var operation = new InventoryOperation(true, count, currentCount, operatorId, description, 0, Id);
+            Operations.Add(operation);
+            IsAvailable = currentCount > 0;
+        }
+
+        public void Decrease(int count, long operatorId, string description, long orderId)
+        {
+            var currentCount = CalculateCurrentCount() - count;
+            var operation = new InventoryOperation(false, count, currentCount, operatorId, description, orderId, Id);
+            Operations.Add(operation);
+            IsAvailable = currentCount > 0;
+        }
+    }
+}
---
+++ InventoryManagement.Domain/InventoryAgg/InventoryOperation.cs
@@ -0,0 +1,27 @@
+﻿using _0_Framework.Domain;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public class InventoryOperation : EntityBase
+    {
+        public int Count { get; private set; }
+        public bool Added { get; private set; }
+        public int CurrentCount { get; private set; }
+        public long OperatorId { get; private set; }
+        public string Description { get; private set; }
+        public long OrderId { get; private set; }
+        public long InventoryId { get; private set; }
+        public Inventory Inventory { get; private set; }
+
+        public InventoryOperation(bool added, int count, int currentCount, long operatorId, string description, long orderId, long inventoryId)
+        {
+            Added = added;
+            Count = count;
+            CurrentCount = currentCount;
+            OperatorId = operatorId;
+            Description = description;
+            OrderId = orderId;
+            InventoryId = inventoryId;
+        }
+    }
+}
