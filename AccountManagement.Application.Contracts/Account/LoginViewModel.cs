43b50a14e4d02b83a9d52ab6d63d76d6b189feb3
AminHosseini <aminhosseini741@gmail.com>
2022-12-28 20:14:25 +0330

    Account Started

---
+++ AccountManagement.Application.Contracts/Account/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AccountManagement.Application.Contracts/Account/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class RegisterViewModel
+    {
+        public string FullName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+    }
+}
